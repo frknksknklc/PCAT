226416adf61d27fb3618794826ebdc6018e80fbb
Furkan Keskinkilic <furkankeskinkilic97@gmail.com>
2021-11-21 22:21:40 +0300

    middleware and template engine added

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 const express = require('express');
-const path = require('path')
+const path = require('path');
+const ejs = require('ejs');
+const { EEXIST } = require('constants');
 
 const app = express();
 
-//middlewares
-app.use(express.static('public'))
-
-app.get('/', (req,res) =>{
-  // const photo= {
-  //   id: 1,
-  //   name: "Photo Name",
-  //   description: "Photo Description"
-  // }
-  // res.send(photo)
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'))
-})
+//Template engine
+app.set('view engine', 'ejs');
+//Middlewares
+app.use(express.static('public'));
+//Routes
+app.get('/', (req, res) => {
+  res.render("index")
+});
+app.get('/about', (req, res) => {
+  res.render("about")
+});
+app.get('/add', (req, res) => {
+  res.render("add")
+});
 
 const port = 3000;
 app.listen(port, () => {
